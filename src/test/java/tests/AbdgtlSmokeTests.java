62bc214d7e3258872f133719516197c43f90d7d0
Anna Gasnikova <however@bk.ru>
2026-09-24 13:18:49 +0400

    Added second smoke test childPagesShouldOpenFromMenu

--- src/test/java/tests/AbdgtlSmokeTests.java
+++ src/test/java/tests/AbdgtlSmokeTests.java
@@ -1,19 +1,19 @@
 package tests;
 
-import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byPlaceholder;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.WebDriverConditions.urlContaining;
+import static com.codeborne.selenide.WebDriverConditions.url;
 import static com.codeborne.selenide.WebDriverConditions.urlStartingWith;
 
 
 public class AbdgtlSmokeTests extends AbdgtlBase {
     @Test
+    @DisplayName("Главная страница открывается")
     void mainPageShouldOpen() {
         open ("");
 
@@ -26,25 +26,39 @@ public class AbdgtlSmokeTests extends AbdgtlBase {
                                 "продукты для удобства клиентов банка"
                 ));
     }
-//    @Test
-//    void mainPageShouldDisplayMainElements() {
-//        open ("");
-//
-//        SelenideElement cookieBanner =
-//                $("[aria-label='Уведомление об использовании файлов cookie']");
-//
-//        if (cookieBanner.isDisplayed()) {
-//            cookieBanner.$("button").click();
-//        }
-//
-//        $(byPlaceholder("Найти событие, артиста или место"))
-//                .shouldBe(visible);
-//
-//        $$("img.tl-image__image[alt]")
-//                .shouldBe(sizeGreaterThan(0))
-//                .first()
-//                .shouldBe(visible);
-//    }
+    @Test
+    @DisplayName("Дочерние страницы открываются из меню")
+    void childPagesShouldOpenFromMenu() {
+        open ("");
+
+        $$("a")
+                .filterBy(exactText("О Нас"))
+                .findBy(visible)
+                .click();
+        webdriver().shouldHave(
+                url("https://abdgtl.com/"));
+
+        $$("a")
+                .filterBy(exactText("Блог"))
+                .findBy(visible)
+                .click();
+        webdriver().shouldHave(
+                url("https://abdgtl.com/blog"));
+
+        $$("a")
+                .filterBy(exactText("Технологии"))
+                .findBy(visible)
+                .click();
+        webdriver().shouldHave(
+                url("https://abdgtl.com/technologies"));
+
+        $$("a")
+                .filterBy(exactText("Продукты и услуги"))
+                .findBy(visible)
+                .click();
+        webdriver().shouldHave(
+                url("https://abdgtl.com/productsandservices"));
+    }
 //    @Test
 //    void eventCardShouldOpenEventPage() {
 //        open("");
